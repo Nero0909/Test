e48d287558554be2d62988fbb0598b09c42f70aa
Nikita Makarov <nero0909@yandex.ru>
2015-01-27 17:17:56 +0400

    Commit

---
+++ GitTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello wordl!");
+            Console.WriteLine("");
+            Console.WriteLine("New word");
+            Console.ReadLine();
+        }
+    }
+}
